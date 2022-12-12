cc77170c82244e30a1e7aff9663f99bb151832a9
avcdsld <avcdsld@gmail.com>
2022-12-13 01:27:16 +0900

    rinkeby -> goerli

--- packages/react-app/src/hooks/useActiveWeb3.ts
+++ packages/react-app/src/hooks/useActiveWeb3.ts
@@ -8,9 +8,9 @@ import { ethers } from "ethers";
 const rpc =
   process.env.NODE_ENV === "test"
     ? // ? "https://polygon-mumbai.infura.io/v3/7495501b681645b0b80f955d4139add9"
-      // : "https://mainnet.infura.io/v3/7495501b681645b0b80f955d4139add9";
-      "https://rinkeby.infura.io/v3/7495501b681645b0b80f955d4139add9"
-    : "https://rinkeby.infura.io/v3/7495501b681645b0b80f955d4139add9";
+    // : "https://mainnet.infura.io/v3/7495501b681645b0b80f955d4139add9";
+    "https://goerli.infura.io/v3/7495501b681645b0b80f955d4139add9"
+    : "https://goerli.infura.io/v3/7495501b681645b0b80f955d4139add9";
 
 export const simpleRpcProvider = new ethers.providers.JsonRpcProvider(rpc);
 /**
--- packages/react-app/src/lib/env.ts
+++ packages/react-app/src/lib/env.ts
@@ -1,6 +1,6 @@
 export const rpc =
   process.env.NODE_ENV === "test"
     ? // ? "https://polygon-mumbai.infura.io/v3/7495501b681645b0b80f955d4139add9"
-      // : "https://mainnet.infura.io/v3/7495501b681645b0b80f955d4139add9";
-      "https://rinkeby.infura.io/v3/7495501b681645b0b80f955d4139add9"
-    : "https://rinkeby.infura.io/v3/7495501b681645b0b80f955d4139add9";
+    // : "https://mainnet.infura.io/v3/7495501b681645b0b80f955d4139add9";
+    "https://goerli.infura.io/v3/7495501b681645b0b80f955d4139add9"
+    : "https://goerli.infura.io/v3/7495501b681645b0b80f955d4139add9";
